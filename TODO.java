be9068367ea2d87e7e76c640bccd80e6ee02347e
Dennis Case <denniscase1999@gmail.com>
2019-02-26 20:06:31 -0600

    Uploading code to GitHub

---
+++ TODO.java
@@ -0,0 +1,21 @@
+
+public class TODO {/*
+Lab Section_________: 13
+Last Modified_______: 2019-02-26
+OU ID_______________: 113467026
+OU EMAIL____________: denniscase1999@ou.edu
+
+Tasks:
+1. 
+2. 
+
+Estimated Time (minutes):
+1. 
+2. 
+
+Actual Time (minutes):
+1. 
+2. 
+
+*/
+}
